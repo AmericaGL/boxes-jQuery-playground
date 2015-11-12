7ae3410d2a30d1f621c9a90e5d5a90083e37f974
AmericaGL <info@cybercodetwins.com>
2015-11-12 01:50:48 -0800

    events in jquery

--- boxes.js
+++ boxes.js
@@ -9,7 +9,24 @@ $(document).ready(function() {
 //4. Change all red boxes to white.
 	$(".boxType1").css("background", "white");
 //Get the first two divs in the second row. Take away all styling from the divs.
-	$("#row2").removeClass("div:");
-// append("<h1>secretbox</h1>")
+	$("#row2").find("div:first").removeClass().next().removeClass();
+//Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
+	$('.box').not('#secretBox').css('width','2px');
+	// append("<h1>secretbox</h1>")
+
+//Add an on click handler to the container div. Console log the x and y position of the click.
+$('#container').click(function(e) {
+  console.log('('+e.pageX+','+e.pageY+')');
+  });
+
 });
+//Add links inside all red box divs that take the user to galvanize. Then add an on click handler that alerts the user that you can never leave the page. 
+//Make sure the user won't leave the page after the alert! */
+$('.boxType1').append('<a href="http://www.galvanize.com">Galvanize</a>').css('position,relative');
+$('a').click(function(e) {
+    //preventDefault...terrible
+     e.preventDefault();
+     alert('You cannot leave the page!');
+});
+
 
